f7c66f91ea7c6e3656f7612d3802f7ca7d444a01
Karlen777 <karltigranovich@gmail.com>
2022-02-13 15:47:48 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let x = prompt("What is you name?");
+let y = prompt("What are you interested?");
+let z = prompt("How old are you?");
+let w = prompt("Are you married?");
+
+let answer = [];
+answer[0] = x ;
+answer[1] = y ;
+answer[2] = z ;
+answer[3] = w ;
+
+console.log(answer);
